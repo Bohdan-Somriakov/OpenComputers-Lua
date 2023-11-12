aca107c6c7a78c933524ad0c9bca4502f2c73161
Pontius-hosting <114800086+Bohdan-Somriakov@users.noreply.github.com>
2023-11-12 22:52:38 +0200

    final_edition_combine_harvester

---
+++ robot_combine_harvester/old version/robot_combine_harvester1.2.lua
@@ -0,0 +1,68 @@
+function sleep(n)
+  os.execute("sleep " .. tonumber(n))
+end
+component = require("component")
+robot = require("robot")
+
+function has_free_slots()
+    local slots_with_items = 0
+    for i = 1, robot.inventorySize() do
+        if robot.count(i) > 0 then
+            slots_with_items = slots_with_items + 1
+        end
+    end
+    if slots_with_items == robot.inventorySize() then
+        return false
+    else
+        return true
+    end
+end
+
+function harvest_crops()
+    local turnRight = true
+
+    while has_free_slots() == true do
+        sleep(5) --> wait till robot recovers its energy
+        robot.useDown()
+        robot.forward()
+
+        if robot.detect() then
+            if turnRight then
+                robot.turnRight()
+                if robot.forward() == nil then
+                    robot.turnAround()
+                end
+                robot.turnRight()
+                turnRight = false
+            else
+                robot.turnLeft()
+                if robot.forward() == nil then
+                    robot.turnAround()
+                end
+                robot.turnLeft()
+                turnRight = true
+            end
+        end
+    end
+end
+
+function move_crops() 
+    if has_free_slots() == false then
+        print("Moving crops to the chest")
+        while robot.detectUp() == false do
+            sleep(5) --> wait till robot recovers its energy
+            if robot.forward() == nil then
+                    robot.turnRight()
+            end
+        end
+        -->
+        -->
+        -->
+        print("Crops have been delivered")
+    end
+end
+
+while true do
+    move_crops()
+    harvest_crops()
+end
